51550bd4e4086b4895e1ab56d34255b0fba48cb8
Philip Forget <philipforget@gmail.com>
2015-02-08 13:15:11 -0500

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = 'AwsSsh',
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'Easily manage ec2 ssh configs',
     author = 'Philip Forget',
     author_email = 'philipforget@gmail.com',
